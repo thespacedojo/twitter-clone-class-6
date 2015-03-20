648b1a9cd3fce05f3504244086bcd25643985658
Josh Owens <joshua.owens@gmail.com>
2015-03-20 14:03:39 -0400

    Add unfollow button code. Fixes #5

--- client/profile.js
+++ client/profile.js
@@ -1,6 +1,9 @@
 Template.profile.events({
   'click .follow-me': function(event, template) {
     Meteor.call('follow', template.data.user._id);
+  },
+  'click .unfollow-me': function(event, template) {
+    Meteor.call('unfollow', template.data.user._id);
   }
 });
 
--- collections/users.js
+++ collections/users.js
@@ -3,5 +3,8 @@ Users = Meteor.users;
 Meteor.methods({
   follow: function(followId) {
     Users.update(this.userId, {$push: {"profile.followingIds": followId}});
+  },
+  unfollow: function(followId) {
+    Users.update(this.userId, {$pull: {"profile.followingIds": followId}});
   }
 });
