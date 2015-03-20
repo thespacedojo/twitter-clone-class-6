b6e909a4e96fa6de0ec45e331491e74ba5282d74
Josh Owens <joshua.owens@gmail.com>
2015-03-20 14:53:51 -0400

    Get started on tweet stream publications. Refs #8

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,13 @@
 Meteor.publish('myTweets', function() {
-  return Tweets.find();
+  userCursor = Users.find({_id: this.userId});
+  user = userCursor.fetch()[0];
+  followingIds = [];
+  followingIds.push(user.profile.followingIds);
+  followingIds.push(user._id);
+  followingIds = _(followingIds).flatten();
+  users = Users.find({_id: {$in: followingIds}}, {fields: {emails: 0, services: 0}});
+  tweets = Tweets.find({userId: {$in: followingIds}});
+  return [users, tweets];
 });
 
 Meteor.publish('profile', function(username) {
