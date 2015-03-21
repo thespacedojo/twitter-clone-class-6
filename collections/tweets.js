880094b6c1d87ca5cf5ecde5d7c1f82f0cbbc7c4
Josh Owens <joshua.owens@gmail.com>
2015-03-21 12:09:34 -0400

    Add mentionIds on the server side. Fixes #10

--- collections/tweets.js
+++ collections/tweets.js
@@ -1,8 +1,26 @@
 Tweets = new Mongo.Collection('tweets');
 
+processTweet = function(text) {
+  if (Meteor.isServer) {
+    var ids = [];
+    if (_.contains(text, "@")) {
+      mentions = _.select(text.split(" "), function(string) {
+        return _.contains(string, "@");
+      });
+      usernames = _.map(mentions, function(mention) {
+        return mention.substring(1);
+      });
+      users = Users.find({username: {$in: usernames}}).fetch();
+      ids = _.pluck(users, "_id");
+    }
+    return ids;
+  }
+};
+
 Tweets.before.insert(function(userId, doc) {
   doc.tweetedAt = new Date();
   doc.userId = userId;
+  doc.mentionIds = processTweet(doc.text);
 });
 
 Tweets.helpers({
