c7839ca161b24b5f9a7507f1ed333029936158bf
Josh Owens <joshua.owens@gmail.com>
2015-03-21 11:24:22 -0400

    Get reactive publications working for the tweet stream. Fixes #8

--- server/publications.js
+++ server/publications.js
@@ -1,13 +1,55 @@
 Meteor.publish('myTweets', function() {
-  userCursor = Users.find({_id: this.userId});
-  user = userCursor.fetch()[0];
-  followingIds = [];
-  followingIds.push(user.profile.followingIds);
-  followingIds.push(user._id);
-  followingIds = _(followingIds).flatten();
-  users = Users.find({_id: {$in: followingIds}}, {fields: {emails: 0, services: 0}});
-  tweets = Tweets.find({userId: {$in: followingIds}});
-  return [users, tweets];
+  var userCursor = Users.find({_id: this.userId});
+  var user = userCursor.fetch()[0];
+  var cursors = [];
+  var ids = [];
+  var self = this;
+  ids.push(user.profile.followingIds);
+  ids.push(self.userId);
+  followingIds = _.flatten(ids);
+  cursors.push(Tweets.find({userId: {$in: followingIds}}));
+  cursors.push(Users.find({
+    _id: {$in: followingIds}
+  }, {
+    fields: {username: 1, "profile.name": 1}
+  }));
+
+  userCursor.observeChanges({
+    changed: function(id, user) {
+      ids = user.profile.followingIds;
+      ids.push(self.userId);
+      console.log(followingIds);
+      flatIds = _.flatten(ids);
+      addedFollowingIds = _.difference(flatIds, followingIds);
+      removedFollowingIds = _.difference(followingIds, flatIds);
+      followingIds = flatIds;
+      // console.log(removedFollowingIds);
+      if (addedFollowingIds) {
+        users = Users.find({_id: {$in: addedFollowingIds}}, {fields: {username: 1, "profile.name": 1}});
+        _.each(users.fetch(), function(user) {
+          console.log(user);
+          self.added('users', user._id, user);
+          tweets = Tweets.find({userId: user._id});
+          tweets.forEach(function(tweet) {
+            console.log(tweet);
+            self.added('tweets', tweet._id, tweet);
+          });
+        });
+      }
+      if (removedFollowingIds) {
+        users = Users.find({_id: {$in: removedFollowingIds}}, {fields: {username: 1, "profile.name": 1}});
+        _.each(users.fetch(), function(user) {
+          self.removed('users', user._id);
+          tweets = Tweets.find({userId: user._id});
+          tweets.forEach(function(tweet) {
+            self.removed('tweets', tweet._id);
+          });
+        });
+      }
+    }
+  });
+
+  return cursors;
 });
 
 Meteor.publish('profile', function(username) {
